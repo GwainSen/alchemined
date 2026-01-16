aaced1700a42433e3a98b7eb8a3411ebcda8b36a
GwainSen <gwainsen@gmail.com>
2026-01-16 22:51:27 +0100

    switched away from depricated function

--- src/main/java/net/gs/alchemined/item/ModItems.java
+++ src/main/java/net/gs/alchemined/item/ModItems.java
@@ -2,9 +2,8 @@ package net.gs.alchemined.item;
 
 import net.gs.alchemined.Alchemined;
 import net.gs.alchemined.block.ModBlocks;
-
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -13,16 +12,16 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Alchemined.MODID);
 
-    public static final DeferredItem<Item> TEST_ITEM1 = ITEMS.registerItem("test_item1", Item::new, new Item.Properties());
+    public static final DeferredItem<Item> TEST_ITEM1 = ITEMS.registerItem("test_item1", Item::new);
 
-    public static final DeferredItem<Item> I_SILVER = ITEMS.registerItem("silver", Item::new, new Item.Properties());
+    public static final DeferredItem<Item> I_SILVER = ITEMS.registerItem("silver", Item::new);
 
-    public static final DeferredItem<Item> I_ELECTRUM= ITEMS.registerItem("electrum", Item::new, new Item.Properties());
+    public static final DeferredItem<Item> I_ELECTRUM= ITEMS.registerItem("electrum", Item::new);
 
     //block items
-    public static final DeferredItem<BlockItem> I_SILVER_ORE= ITEMS.registerSimpleBlockItem("silver_ore", ModBlocks.B_SILVER_ORE, new Item.Properties());
-    public static final DeferredItem<BlockItem> I_SILVER_BLOCK= ITEMS.registerSimpleBlockItem("silver_block", ModBlocks.B_SILVER_BLOCK, new Item.Properties());
-    public static final DeferredItem<BlockItem> I_ELECTRUM_BLOCK= ITEMS.registerSimpleBlockItem("electrum_block", ModBlocks.B_ELECTRUM_BLOCK, new Item.Properties());   
+    public static final DeferredItem<BlockItem> I_SILVER_ORE= ITEMS.registerSimpleBlockItem("silver_ore", ModBlocks.B_SILVER_ORE);
+    public static final DeferredItem<BlockItem> I_SILVER_BLOCK= ITEMS.registerSimpleBlockItem("silver_block", ModBlocks.B_SILVER_BLOCK);
+    public static final DeferredItem<BlockItem> I_ELECTRUM_BLOCK= ITEMS.registerSimpleBlockItem("electrum_block", ModBlocks.B_ELECTRUM_BLOCK);   
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
