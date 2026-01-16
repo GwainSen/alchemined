d9922494a20c407a02f22d4e0b08498a167a23ea
GwainSen <gwainsen@gmail.com>
2026-01-16 16:05:42 +0100

    added tab translation

--- src/main/java/net/gs/alchemined/item/ModCreativeModeTabs.java
+++ src/main/java/net/gs/alchemined/item/ModCreativeModeTabs.java
@@ -1,16 +1,13 @@
 package net.gs.alchemined.item;
 
 import net.gs.alchemined.Alchemined;
-
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.resources.Identifier;
 
 
 
@@ -21,7 +18,7 @@ public class ModCreativeModeTabs {
     public static final DeferredHolder<CreativeModeTab,CreativeModeTab> ALCHEMINED_ITEMS = CREATIVE_MODE_TAB
         .register("alchemined_items_tab",
         () -> CreativeModeTab.builder()
-        .title(Component.translatable("creativetab.alchemined_items"))
+        .title(Component.translatable("creativetab.alchemined.alchemined_items"))
         .withTabsBefore(CreativeModeTabs.COMBAT)
         .icon(() -> ModItems.I_ELECTRUM.get().getDefaultInstance())
         .displayItems((parameters, output) -> {
